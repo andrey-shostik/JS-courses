32e8f811ce3f2f8d39dae17c6cf77f8b6c8253e6
Andrey Kolomoets <kolomoets.and@gmail.com>
2018-04-03 23:04:24 +0300

    Update jsTasks.js

--- jsTasks.js
+++ jsTasks.js
@@ -1,16 +1,13 @@
 // реализовать функцию которая вернет сумму элементов произвольного массива
 
 var arr = [1, 10, 15, 25, 40];
-
 function arraySum(array) {
     var sum = 0;
     for (var i = 0; i < array.length; i++) {
         sum += array[i];
     }
-
     return sum
 }
-
 console.log(arraySum(arr));
 
 // реализовать функцию которая принимает произвольный массив и возвращает отсортированный по возростанию, 
@@ -21,11 +18,10 @@ console.log(arraySum(arr));
 function sortFunction(a, b) {
     if (a > b) return 1;
     if (a < b) return -1;
+    return 0;
 }
-  
 var arr = [ 4, 1, 45, 6, 37, 554, 2 ];
 arr.sort(sortFunction);
-  
 console.log(arr);
 
 // или
@@ -33,16 +29,21 @@ console.log(arr);
 function sortFunction(a, b) {
     return a - b;
 }
-  
 var arr = [ 4, 1, 45, 6, 37, 554, 2 ];
 arr.sort(sortFunction);
- 
+console.log(arr);
+
+// или
+
+var arr = [4, 1, 45, 6, 37, 554, 2];
+arr.sort(function (a, b) {
+    return a - b;
+});
 console.log(arr);
 
 // b) цикл for:
 
 var arr = [ 4, 1, 45, 6, 37, 554, 2];
-    
 for (a = 0; a < arr.length; a++) {
     for (b = 0; b < arr.length; b++) {
         if (arr [a] < arr [b]) {
@@ -52,17 +53,14 @@ for (a = 0; a < arr.length; a++) {
         } 
     }
 }   
-  
 console.log(arr);
 
 // реализовать функцию которая возвращает массив с уникальными элементами // [1,1,2,3,1,2] => [1,2,3]
 
 var arr = [ 1, 1, 1, 2, 3, 1, 2, 4, 4, 10 ];
-
 function uniqueVal(value, index, self) { 
     return self.indexOf(value) === index;
 }
-
 console.log( arr.filter( uniqueVal) );
 
 // реализовать функцию которая возвращает массив с числами фибоначчи, 
