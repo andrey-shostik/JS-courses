4e8f3a9d51bb18e921862ab87c605fcacc062a93
Aleksandr Ovchinnikov <ovchinnikov.alexandr.ua@gmail.com>
2018-04-05 23:35:08 +0300

    4-th task

--- main.js
+++ main.js
@@ -20,13 +20,38 @@ console.log(arr2.sort(ascending));
 var arr3 = [1,1,2,3,1,2,3];
 function unic (arr3) {
     var obj = {};
-    for(var i = 0; i<arr3.length; i++){
+    for(var i = 0; i<arr3.length; i++) {
         var num = arr3[i];
         obj[num] = true;
     }
+    var result = [];
+    for(var j = 0; j<arr3.length; j++){
+        var value = arr3[j];
+         if (obj[value] == true){
+             result.push(value);
+             obj[value] = false;
+         }
 
-
-    return Object.keys(obj);
+    }
+    return result;
 };
 
 console.log(unic(arr3));
+
+function getFibValues(a) { // не правильно работает с 0 1 2
+    var fibvalues = [];
+    for (var i = 0; i<3; i++) {
+        if (i == 0 || i == 1) {
+            fibvalues.push(1);
+        }
+    }
+
+        for (i = 3; i < a; i++){
+            var first = fibvalues[i-2];/
+            var second = fibvalues[i-3];
+            var next = first + second;
+            fibvalues.push(next);
+        }
+    return fibvalues;
+    }
+console.log(getFibValues(0));
