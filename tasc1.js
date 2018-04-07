65c991a8892c9df3f22e3d9e2e65290d105cf299
ElistratovSergey <elistratovs@gmaol.com>
2018-04-07 23:54:58 +0300

    реализовал функцию которая вернет сумму элементов произвольного массива

---
+++ tasc1.js
@@ -0,0 +1,9 @@
+
+const sum = (collection) =>
+    function getSumArray() {
+        let sum = 0;
+        for (let i = 0; i < arr.length; i++) {
+            sum += collection[index];
+        }
+    }
+return sum;
---
+++ tasc2.js
@@ -0,0 +1,23 @@
+через метод sort:
+    function compareArr(a, b) {
+        if (a > b) return 1;
+        if (a < b) return -1;
+    }
+arr.sort(compareArr);
+
+console.log(arr);
+
+
+через цикл for:
+
+function sortArrFor() {
+    for (var i = 0; i < arr.length; i++) {
+        for (var s = 0; s < arr.length; s++) {
+            if (arr[s] > arr[s + 1]) {
+                var sort = arr[s];
+                arr[s] = arr[s + 1];
+                arr[s + 1] = sort;
+            }
+        }
+    }
+    return arr;
