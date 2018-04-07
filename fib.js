a2fc39c7d23909e6eceaebace31b1a60a79b8aeb
Vlad Horbenko <36421109+valkyriy@users.noreply.github.com>
2018-04-07 22:36:15 +0300

    4.

---
+++ fib.js
@@ -0,0 +1,11 @@
+
+function fibi(n) {
+    var sum = 0,i,str,arr=[];
+    for (i-0; i<n; i++){
+        if( arr.length > 1)  sum += arr[ arr.length-2];
+        if( arr.length == 1) sum = 1;
+        if( arr.length == 0) sum = 0;
+arr.push( sum);
+    str=arr.join(",");
+    }
+    }
