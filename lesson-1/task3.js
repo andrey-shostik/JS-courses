a1bc8d34cdc53967530d54ae89fe758401818680
Olexandr Gorkavenko <gorkavenko.olexandr@gmail.com>
2018-04-10 09:19:10 +0300

    const unSortArr renamed to notUniqArr; instead of the value of the property (obj[key]) we pass into the uniqArr the name of the property (key).

--- lesson-1/task3.js
+++ lesson-1/task3.js
@@ -1,23 +1,23 @@
 // Задание 3: реализовать функцию которая возвращает массив с уникальными элементами
 // [1,1,2,3,1,2] => [1,2,3]
 
-const unSortArr = [1, 1, 2, 3, 1, 2];
+const notUniqArr = [1, 1, 2, 3, 1, 2];
 
-function uniqueValues(unSortArr) {
+function uniqueValues(notUniqArr) {
     let obj = {};
 
-    for (let i = 0; i < unSortArr.length; i++) {
-        let temp = unSortArr[i];
+    for (let i = 0; i < notUniqArr.length; i++) {
+        let temp = notUniqArr[i];
         obj[temp] = true;
     }
 
     let uniqArr = [];
 
     for (let key in obj) {
-        uniqArr.push(obj[key]);
+        uniqArr.push(key);
     }
 
     return uniqArr;
 }
 
-console.log('Unique array values: ' + uniqueValues(unSortArr));
+console.log('Unique array values: ' + uniqueValues(notUniqArr));
