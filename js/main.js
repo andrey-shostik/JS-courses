a8614b6f2519d72679f23c966eb4a4081d266662
klugmankd <klugmankd@gmail.com>
2018-04-11 18:19:08 +0300

    homework-1 by Kostya Klugman fixed issues

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const sum = (collection) => {
+const getSum = (collection) => {
     let sum = 0;
 
     for (let index in collection) {
@@ -12,7 +12,7 @@ const bubbleSort = (collection) => {
     for (let outerIndex = 0; outerIndex < collection.length; outerIndex++) {
         for (let innerIndex = 0; innerIndex < collection.length - 1; innerIndex++) {
             if (collection[innerIndex] > collection[innerIndex + 1]) {
-                let current = collection[innerIndex];
+                const current = collection[innerIndex];
                 collection[innerIndex] = collection[innerIndex + 1];
                 collection[innerIndex + 1] = current
             }
@@ -34,7 +34,7 @@ const unique = (collection) => {
 };
 
 const uniqueBySet = (collection) => {
-    return Array.from(new Set(collection));
+    Array.from(new Set(collection))
 };
 
 const fibonacci = (count, arrayCount) => {
@@ -43,7 +43,7 @@ const fibonacci = (count, arrayCount) => {
         resultArray = [];
 
     for (let i = 3; i <= count; i++) {
-        let c = a + b;
+        const c = a + b;
         a = b;
         b = c;
         resultArray.push(b)
@@ -56,9 +56,8 @@ const fibonacci = (count, arrayCount) => {
 const array = [5, 3, 6, 2, 2, 3, 5, 4];
 
 
-
-console.log("sum of array elements: ");
-console.log(sum(array));
+console.log("getSum of array elements: ");
+console.log(getSum(array));
 
 console.log("sorted array:");
 console.log(bubbleSort(array));
