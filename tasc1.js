a8ccb3c779cb6e3e8fd1877463a670c74c0bc8fe
ElistratovSergey <elistratovs@gmaol.com>
2018-04-15 15:50:41 +0300

    Исправил, согласно замечаниям.

--- tasc1.js
+++ tasc1.js
@@ -1,9 +1,8 @@
-
-const sum = (collection) =>
-    function getSumArray() {
-        let sum = 0;
-        for (let i = 0; i < arr.length; i++) {
-            sum += collection[index];
-        }
+// Реализовать функцию которая вернет сумму элементов произвольного массива
+function getSumArr(collection) {
+    let sum = 0;
+    for (let i = 0; i < collection.length; i++) {
+        sum += collection[i];
     }
-return sum;
+    return sum;
+}
