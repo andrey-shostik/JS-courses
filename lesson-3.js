1082ccd6d3c34b303e3bef43507722c06d38f547
Andrey Kolomoets <kolomoets.and@gmail.com>
2018-04-16 15:35:32 +0300

    Update lesson-3.js

--- lesson-3.js
+++ lesson-3.js
@@ -14,15 +14,26 @@ function getPartialSumArray(arr) {
 }
 console.log( getPartialSumArray([3,12,4,1,156,42,17]) );
 
-// задание 2: Реализовать функцию которая вернет новый массив на основе этого https://pastebin.com/0xvghaeT, элементы
-// которого должны быть объектами и иметь поля id, name и avaragePrice - среднее арифметическое от суммы
-// firstPrice и secondPrice, нужно использовать метод map.
+/* задание 2: Реализовать функцию которая вернет новый массив на основе этого https://pastebin.com/0xvghaeT, элементы
+   которого должны быть объектами и иметь поля id, name и avaragePrice - среднее арифметическое от суммы
+   firstPrice и secondPrice, нужно использовать метод map. */
 
 
 
 
-// задание 3: Реализовать функцию которая вернет массив с элементами в которых avaragePrice > 5000, элементы массива 
-// должны быть отсортированны по полю avaragePrice, по возростанию. Массив брать из вызова функции задания 2, 
-// нужно использовать методы filter и sort.
+/* задание 3: Реализовать функцию которая вернет массив с элементами в которых avaragePrice > 5000, элементы массива 
+   должны быть отсортированны по полю avaragePrice, по возростанию. Массив брать из вызова функции задания 2, 
+   нужно использовать методы filter и sort. */
 
 
+
+
+/* Деструктурировать свойства из JSON:
+   requestId,
+   result.accountNumber,
+   result.balance.otb,
+   result.cards.ucid,
+   result.cards.expireDate,
+   result.cards.expireDate.year,
+   result.overdraftInfo.lastCloseDayOverBalance,
+   result.bank.id */
