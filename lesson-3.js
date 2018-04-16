b264e2922d9ac261ee6e9718031ff7672d4a18c0
Andrey Kolomoets <kolomoets.and@gmail.com>
2018-04-16 10:48:22 +0300

    lesson-3.js
    Решено задание №1.

---
+++ lesson-3.js
@@ -0,0 +1,28 @@
+// задание 1: Реализовать функцию которая возвращает массив его частичных сумм c помощью reduce // [1,2,3,4,5] => [1, 3, 6, 10, 15]
+
+function getPartialSumArray(arr) {
+  var result = [];
+    if (!arr.length) return result;
+
+  var totalSum = arr.reduce(function(sum, item) {
+    result.push(sum);
+    return sum + item;
+  });
+  
+  result.push(totalSum);
+  return result;
+}
+console.log( getPartialSumArray([3,12,4,1,156,42,17]) );
+
+// задание 2: Реализовать функцию которая вернет новый массив на основе этого https://pastebin.com/0xvghaeT, элементы
+// которого должны быть объектами и иметь поля id, name и avaragePrice - среднее арифметическое от суммы
+// firstPrice и secondPrice, нужно использовать метод map.
+
+
+
+
+// задание 3: Реализовать функцию которая вернет массив с элементами в которых avaragePrice > 5000, элементы массива 
+// должны быть отсортированны по полю avaragePrice, по возростанию. Массив брать из вызова функции задания 2, 
+// нужно использовать методы filter и sort.
+
+
