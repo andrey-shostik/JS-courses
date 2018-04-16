d3782a3338b1b1420db1d2b540868466c761f36e
Andrey Kolomoets <kolomoets.and@gmail.com>
2018-04-16 17:50:12 +0300

    Update lesson-3.js

--- lesson-3.js
+++ lesson-3.js
@@ -18,7 +18,29 @@ console.log( getPartialSumArray([3,12,4,1,156,42,17]) );
    которого должны быть объектами и иметь поля id, name и avaragePrice - среднее арифметическое от суммы
    firstPrice и secondPrice, нужно использовать метод map. */
 
+var sourceArray = [
+{"id":1,"name":"Sprinter 2500","firstPrice":8172,"secondPrice":9273},
+{"id":2,"name":"Touareg","firstPrice":5856,"secondPrice":5130},
+{"id":3,"name":"Avenger","firstPrice":8674,"secondPrice":3118},
+{"id":4,"name":"Tracer","firstPrice":8406,"secondPrice":2382},
+{"id":5,"name":"Durango","firstPrice":2093,"secondPrice":6622},
+{"id":6,"name":"968","firstPrice":7735,"secondPrice":3681},
+{"id":7,"name":"Golf","firstPrice":9363,"secondPrice":7039},
+{"id":8,"name":"Z4","firstPrice":4827,"secondPrice":6776},
+{"id":9,"name":"LaCrosse","firstPrice":3169,"secondPrice":5229},
+{"id":10,"name":"Insight","firstPrice":9030,"secondPrice":9207}
+];
 
+var resultArr = [];
+
+for (var {"id":id, "name":name, "firstPrice":firstPrice, "secondPrice":secondPrice} of sourceArray) {
+	function average() {
+    var sum = 0;
+      for (var i = 0; i < arguments[i]; i++ ) sum += arguments[i];
+      return sum == 0 ? sum : sum / arguments.length;
+  }
+console.log("ID: " + id + ", NAME: " + name + ", Average Price: " + average (firstPrice, secondPrice));
+}
 
 
 /* задание 3: Реализовать функцию которая вернет массив с элементами в которых avaragePrice > 5000, элементы массива 
