3d64f1c8fa0d9cef1da95ceb70098a77b9280f04
Andrey Kolomoets <kolomoets.and@gmail.com>
2018-04-18 02:03:05 +0300

    update lesson-3.js
    выполнено задание №4

--- lesson-3.js
+++ lesson-3.js
@@ -65,3 +65,92 @@ console.log(averagePriceArray);
    result.cards.expireDate.year,
    result.overdraftInfo.lastCloseDayOverBalance,
    result.bank.id */
+
+const jsonDataObject = {
+  "requestId" : "eed3fc9d337261ea23f0",
+  "result" : [ {
+    "accountNumber" : "40802810800000018334",
+    "agreementNumber" : "7000351523",
+    "type" : 14,
+    "status" : "NORM",
+    "tariffCode" : "TFLE1.1",
+    "createdOn" : "2016-09-14T00:00:00",
+    "currency" : "643",
+    "mainFlag" : "Y",
+    "balance" : {
+      "balance" : 0,
+      "otb" : 0,
+      "authorized" : 0,
+      "pendingPayments" : 0,
+      "pendingRequisitions" : 0
+    },
+    "newBalance" : {
+      "balance" : 0,
+      "otb" : 0,
+      "authorized" : 0,
+      "pendingPayments" : 0,
+      "pendingRequisitions" : 0,
+      "techOver" : 0
+    },
+    "stats" : {
+      "debit" : 0,
+      "credit" : 0,
+      "interest" : 0
+    },
+    "cards" : [ {
+      "number" : "553420******3894",
+      "expireDate" : {
+        "year" : 2021,
+        "month" : 9
+      },
+      "embossedName" : "DAVID UBILAVA",
+      "name" : "DAVID UBILAVA",
+      "isActive" : true,
+      "ucid" : 1024452429,
+      "status" : "BAML",
+      "ownedByCurrentUser" : true
+    } ],
+    "overIsAvailable" : 0,
+    "overdraftInfo" : {
+      "amount" : 0,
+      "clientIsInOver" : 0,
+      "lastCloseDayOverBalance" : 0,
+      "currentVirtualOverBalance" : 0,
+      "fullOverBalance" : 0
+    },
+    "activationDate" : "2016-09-16T00:00:00",
+    "bank" : {
+      "id" : 99,
+      "name" : "tcs-sme",
+      "bic" : "044525974",
+      "isExternal" : false
+    }
+  } ],
+  "success" : true
+};
+
+console.log(jsonDataObject);
+
+const { requestId } = jsonDataObject;
+console.log("requestId: " + requestId);
+
+const { result: [ { accountNumber } ] } = jsonDataObject;
+console.log("accountNumber: " + accountNumber);
+
+const { result: [ { balance: { otb } } ] } = jsonDataObject;
+console.log("otb: " + otb);
+
+const { result: [ { cards: [ { ucid } ] } ] } = jsonDataObject;
+console.log("ucid: " + ucid);
+
+const { result: [ { cards: [ { expireDate: { year } } ] } ] } = jsonDataObject;
+console.log("year: " + year);
+
+const { result: [ { cards: [ { expireDate: { month } } ] } ] } = jsonDataObject;
+console.log("month: " +  month);
+
+const { result: [ { overdraftInfo: { lastCloseDayOverBalance } } ] } = jsonDataObject;
+console.log("lastCloseDayOverBalance: " + lastCloseDayOverBalance);
+
+const { result: [ { bank: { id } } ] } = jsonDataObject;
+console.log("id: " + id);
