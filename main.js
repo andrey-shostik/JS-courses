6d0dc41f679e75cd88cac9625a00e8b4b5388d58
Klyuchko <klyuch151@gmail.com>
2018-04-17 23:16:25 +0300

    Task 1

--- main.js
+++ main.js
@@ -1,70 +1,18 @@
+// Задача 1
 
-//Задача 1
+const arry = [1, 2, 3, 4, 5];
 
-var array = [1, 3, 5, 7];
-var arraySum = 0;
+function getSums(arr) {
+    let result = [];
+    if (!arr.length) return result;
 
-array.forEach(function (item) {
-    arraySum += item;
-});
+    let totalSum = arr.reduce(function (sum, item) {
+        result.push(sum);
+        return sum + item;
+    });
+    result.push(totalSum);
 
-console.log(arraySum);
-
-
-//Задача 2
-
-var arrayOfNumbers = [10, 3, 1, 9, 2, 22];
-
-arrayOfNumbers.sort(function(a, b){
-    return a-b
-});
-
-console.log(arrayOfNumbers);
-
-
-//Задача 2.2
-
-function sortingArray(arrayOfNumbers) {
-    var n = arrayOfNumbers.length;
-    for (let i = 0; i < n; i++) {
-        for (let j = i+1; j < n; j++) {
-            if (arrayOfNumbers[i] > arrayOfNumbers[j]) {
-                j = arrayOfNumbers[i];
-                arrayOfNumbers[i] = arrayOfNumbers[j];
-                arrayOfNumbers[j] = j;
-                }
-        }
-    }
-    return arrayOfNumbers;
+    return result;
 }
-console.log(sortingArray(arrayOfNumbers));
-
-//Задача 3
-
-var arr = [1,1,2,3,1,2];
-
-function returnUniqueVal(value, index, self) {
-    return self.indexOf(value) == index;
-}
-console.log( arr.filter( returnUniqueVal) );
-
-
-//Задача 4
-
-function getFibonachiNumbers(n) {
-    var firstNum = 1;
-    var secondNum = 1;
-    var nextNum = 3;
-    for (nextNum; nextNum <= n; nextNum++) {
-        var sum = firstNum + secondNum;
-        firstNum = secondNum;
-        secondNum = sum;
-    }
-    return secondNum;
-}
-console.log(getFibonachiNumbers (5));
-console.log(getFibonachiNumbers (10));
-console.log(getFibonachiNumbers (30));
-
-
+console.log( getSums(arry) );
 
