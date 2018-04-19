58070aee597b32a7760f6b34f8b93aa2e2d56987
Vladislav-Oleksienko <splinfao1@gmail.com>
2018-04-19 21:38:18 +0300

    Added homework

--- lesson3.js
+++ lesson3.js
@@ -1,76 +1,132 @@
-// Задание 1.Реализовать функцию которая вернет сумму элементов произвольного массива
+///////////////// #1 \\\\\\\\\\\\\\\\
 
-let sumElements = function() {
-  let sum = 0;
+const array = [ 1, 2, 3, 4, 5 ];
 
-  for(let index = 0; index < arguments.length; index++) {
-      sum += arguments[index];
-  }
-  return sum;
+function getSums(arr1) {
+  let result = [];
+  array.reduce(function (previous,current) {
+    result.push(previous + current);
+    return previous + current;
+  }, 0)
+  return result;
 }
 
-console.log(sumElements(1,2,3,4));
+  console.log(getSums(array));
 
-// Задание 2.Реализовать функцию которая принимает произвольный массив и возвращает отсортированный по возрастанию через цикл for.
 
-function getSortedArray() {
-  let array = [1,3,-1,0,2,13],
-      index ,firstValue,secondValue,thirdValue = array.length;
+///////////////// #2 \\\\\\\\\\\\\\\\
 
-  for (firstValue = 0; firstValue < thirdValue; firstValue++) {
-      for(index = 0; index < thirdValue-1; index++) {
-        if(array[index] > array[index+1]) {
-            secondValue = array[index];
-            array[index] = array[index+1];
-            array[index+1] = secondValue;
-      }
+const infoCarsArray = [
+    {"id":1,"name":"Sprinter 2500","firstPrice":8172,"secondPrice":9273},
+    {"id":2,"name":"Touareg","firstPrice":5856,"secondPrice":5130},
+    {"id":3,"name":"Avenger","firstPrice":8674,"secondPrice":3118},
+    {"id":4,"name":"Tracer","firstPrice":8406,"secondPrice":2382},
+    {"id":5,"name":"Durango","firstPrice":2093,"secondPrice":6622},
+    {"id":6,"name":"968","firstPrice":7735,"secondPrice":3681},
+    {"id":7,"name":"Golf","firstPrice":9363,"secondPrice":7039},
+    {"id":8,"name":"Z4","firstPrice":4827,"secondPrice":6776},
+    {"id":9,"name":"LaCrosse","firstPrice":3169,"secondPrice":5229},
+    {"id":10,"name":"Insight","firstPrice":9030,"secondPrice":9207}
+];
+
+function getArrayWithAvaragePrices(array){
+  let arrayWithAvaragePrices = [];
+  arrayWithAvaragePrices = array.map(function (options) {
+    let {id,name,firstPrice,secondPrice} = options;
+    let avaragePrice = (firstPrice + secondPrice) / 2;
+    return {id,name,avaragePrice};
+  })
+  return arrayWithAvaragePrices;
+}
+console.log(getArrayWithAvaragePrices(infoCarsArray));
+
+///////////////// #3 \\\\\\\\\\\\\\\\
+
+function getFiltered(arr) {
+  let positiveArray = arr.filter(function (item) {
+    return item.avaragePrice > 5000;
+  })
+  let sorted = positiveArray.sort(function (a,b) {
+    return a.avaragePrice - b.avaragePrice;
+  })
+  return sorted;
+}
+const filters = getArrayWithAvaragePrices(infoCarsArray);
+console.log(getFiltered(filters));
+
+
+///////////////// #4 \\\\\\\\\\\\\\\\
+
+let informationAboutUser = {
+  "requestId" : "eed3fc9d337261ea23f0",
+  "result" : [ {
+    "accountNumber" : "40802810800000018334",
+    "agreementNumber" : "7000351523",
+    "type" : 14,
+    "status" : "NORM",
+    "tariffCode" : "TFLE1.1",
+    "createdOn" : "2016-09-14T00:00:00",
+    "currency" : "643",
+    "mainFlag" : "Y",
+    "balance" : {
+      "balance" : 0,
+      "otb" : 0,
+      "authorized" : 0,
+      "pendingPayments" : 0,
+      "pendingRequisitions" : 0
+    },
+    "newBalance" : {
+      "balance" : 0,
+      "otb" : 0,
+      "authorized" : 0,
+      "pendingPayments" : 0,
+      "pendingRequisitions" : 0,
+      "techOver" : 0
+    },
+    "stats" : {
+      "debit" : 0,
+      "credit" : 0,
+      "interest" : 0
+    },
+    "cards" : [ {
+      "number" : "553420******3894",
+      "expireDate" : {
+        "year" : 2021,
+        "month" : 9
+      },
+      "embossedName" : "DAVID UBILAVA",
+      "name" : "DAVID UBILAVA",
+      "isActive" : true,
+      "ucid" : 1024452429,
+      "status" : "BAML",
+      "ownedByCurrentUser" : true
+    } ],
+    "overIsAvailable" : 0,
+    "overdraftInfo" : {
+      "amount" : 0,
+      "clientIsInOver" : 0,
+      "lastCloseDayOverBalance" : 0,
+      "currentVirtualOverBalance" : 0,
+      "fullOverBalance" : 0
+    },
+    "activationDate" : "2016-09-16T00:00:00",
+    "bank" : {
+      "id" : 99,
+      "name" : "tcs-sme",
+      "bic" : "044525974",
+      "isExternal" : false
     }
-  }
-  return array;
-}
-console.log(getSortedArray());
-
-// Задание 2.1. Реализовать функцию которая принимает произвольный массив и возвращает отсортированный по возрастанию через sort.
-const numArray = [1,5,2,-1,7,8,3];
-
-let sorted = numArray.sort(function (a, b) {
-    return a - b;
-});
-console.log(sorted);
-
-// Задание 3. Реализовать функцию которая возвращает массив с уникальными элементами
-
-let getUniqueElements = function (arr) {
-    let newArr = [];
-    let index = arr.length - 1;
-    let test = {};
-
-    for (; index >= 0; index--) {
-        if (arr [index] in test) continue;
-        newArr.push(arr[index]);
-        test[arr[index]] = 1;
-    }
-    return newArr;
+  } ],
+  "success" : true
 }
 
-let arr = ['1', 'Слово', '1', 'Слово', '2', '2'];
-console.log(getUniqueElements(arr));
+const {requestId} = informationAboutUser;
+const {result: [ {accountNumber} ] } = informationAboutUser;
+const {result: [ {balance: {otb} } ] } = informationAboutUser;
+const {result: [ {cards: [ {ucid} ] } ] } = informationAboutUser;
+const {result: [ {cards: [ {expireDate} ] } ] } = informationAboutUser;
+const {result: [ {cards: [ {expireDate: {year} } ] } ] }  = informationAboutUser;
+const {result:[ {overdraftInfo: {lastCloseDayOverBalance} } ] } = informationAboutUser;
+const {result: [ {bank: {id} } ] } = informationAboutUser;
 
-// Задание 4. Реализовать функцию которая возвращает массив с числами фибоначчи, количество которых зависит от переданного значения при вызове функции
-
-function fibonacci(n) {
-    const newArray = [];
-    newArray[0] = 1;
-    newArray[1] = 1;
-
-    if (n <= 1 ) {
-      return 1;
-    } else {
-      for (let index = 2; index <n ; index++) {
-      newArray[index] = newArray[index-1] + newArray[index-2];
-    }
-  }
-    return newArray;
-}
-
-console.log(fibonacci(7));
+console.log(requestId, accountNumber, otb, ucid, expireDate, year, lastCloseDayOverBalance, id);
