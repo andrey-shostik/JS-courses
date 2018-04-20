fed0b6d5d9c547c61f3d29fbe591f2fb6d1806c4
Klyuchko <klyuch151@gmail.com>
2018-04-20 17:39:14 +0300

    Task 4

--- main.js
+++ main.js
@@ -51,3 +51,86 @@ function arryFilterSort(arr) {
     return sortUP
 }
 console.log(arryFilterSort(avaregePrice(arryCars)));
+
+// Задача 3
+
+const JSON = {
+    "requestId" : "eed3fc9d337261ea23f0",
+    "result" : [ {
+        "accountNumber" : "40802810800000018334",
+        "agreementNumber" : "7000351523",
+        "type" : 14,
+        "status" : "NORM",
+        "tariffCode" : "TFLE1.1",
+        "createdOn" : "2016-09-14T00:00:00",
+        "currency" : "643",
+        "mainFlag" : "Y",
+        "balance" : {
+            "balance" : 0,
+            "otb" : 0,
+            "authorized" : 0,
+            "pendingPayments" : 0,
+            "pendingRequisitions" : 0
+        },
+        "newBalance" : {
+            "balance" : 0,
+            "otb" : 0,
+            "authorized" : 0,
+            "pendingPayments" : 0,
+            "pendingRequisitions" : 0,
+            "techOver" : 0
+        },
+        "stats" : {
+            "debit" : 0,
+            "credit" : 0,
+            "interest" : 0
+        },
+        "cards" : [ {
+            "number" : "553420******3894",
+            "expireDate" : {
+                "year" : 2021,
+                "month" : 9
+            },
+            "embossedName" : "DAVID UBILAVA",
+            "name" : "DAVID UBILAVA",
+            "isActive" : true,
+            "ucid" : 1024452429,
+            "status" : "BAML",
+            "ownedByCurrentUser" : true
+        } ],
+        "overIsAvailable" : 0,
+        "overdraftInfo" : {
+            "amount" : 0,
+            "clientIsInOver" : 0,
+            "lastCloseDayOverBalance" : 0,
+            "currentVirtualOverBalance" : 0,
+            "fullOverBalance" : 0
+        },
+        "activationDate" : "2016-09-16T00:00:00",
+        "bank" : {
+            "id" : 99,
+            "name" : "tcs-sme",
+            "bic" : "044525974",
+            "isExternal" : false
+        }
+    } ],
+    "success" : true
+};
+/*
+requestId,
+result.accountNumber,
+result.balance.otb,
+result.cards.ucid,
+result.cards.expireDate
+result.cards.expireDate.year,
+result.overdraftInfo.lastCloseDayOverBalance,
+result.bank.id
+ */
+const {requestId} = JSON;
+const {result: [{accountNumber}]} = JSON;
+const {result: [{balance: {otb}}]} = JSON;
+const {result: [{cards: {ucid}}]} = JSON;
+const {result: [{cards: {expireDate}}]} = JSON;
+const {result: [{cards: [{expireDate: {year}}]}]} = JSON;
+const {result: [{overdraftInfo: {lastCloseDayOverBalance}}]} = JSON;
+const {result: [{bank: {id}}]} = JSON;
