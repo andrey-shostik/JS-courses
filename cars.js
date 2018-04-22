d22584462af0601b33874116cb9b709601039b1d
Николай <nick.melni4uk@gmail.com>
2018-04-22 08:50:39 +0300

    Lesson 3. Tasks 1,2,3

--- cars.js
+++ cars.js
@@ -1,4 +1,5 @@
 massOfCar = [
+
     {"id":1,"name":"Sprinter 2500","firstPrice":8172,"secondPrice":9273},
     {"id":2,"name":"Touareg","firstPrice":5856,"secondPrice":5130},
     {"id":3,"name":"Avenger","firstPrice":8674,"secondPrice":3118},
@@ -9,4 +10,5 @@ massOfCar = [
     {"id":8,"name":"Z4","firstPrice":4827,"secondPrice":6776},
     {"id":9,"name":"LaCrosse","firstPrice":3169,"secondPrice":5229},
     {"id":10,"name":"Insight","firstPrice":9030,"secondPrice":9207}
+
 ];
