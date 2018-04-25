67b11a42b8f961f3257e3e7734165c1768281788
Andrey Kolomoets <kolomoets.and@gmail.com>
2018-04-26 01:08:04 +0300

    Add task-5 & task-6 .js files

---
+++ task-5.js
@@ -0,0 +1,9 @@
+/* Задание №5. Реализуйте класс Validator, который будет проверять строки. К примеру, у него будет метод isEmail 
+параметром принимает строку и проверяет, является ли она корректным емейлом или нет. Если является - возвращает true, 
+если не является - то false. Кроме того, класс будет иметь следующие методы: метод isDomain для проверки домена, 
+метод isDate для проверки даты и метод isPhone для проверки телефона:
+var validator = new Validator();
+console.log(validator.isEmail('phphtml@mail.ru'));
+console.log(validator.isDomain('phphtml.net'));
+console.log(validator.isDate('12.05.2020'));
+console.log(validator.isPhone('+375 (29) 817-68-92')); //тут можете формат своей страны */
