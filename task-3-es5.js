7f64029db84aa5553f46ff084957027a82ebd0d6
Andrey Kolomoets <kolomoets.and@gmail.com>
2018-04-26 00:25:21 +0300

    Add tasks 1,2,3
    добавлены файлы с выполненными заданиями 1-3 (ES5 & ES6)

---
+++ task-3-es5.js
@@ -0,0 +1,52 @@
+/* Задание №3. Модифицируйте класс Worker из предыдущей задачи следующим образом: 
+для свойства rate и для свойства days сделайте еще и методы-сеттеры. Наш класс теперь будет работать так:
+var worker = new Worker('Иван', 'Иванов', 10, 31);
+console.log(worker.getRate()); //выведет 10
+console.log(worker.getDays()); //выведет 31
+console.log(worker.getSalary()); //выведет 310 - то есть 10*31
+//Теперь используем сеттер:
+worker.setRate(20); //увеличим ставку
+worker.setDays(10); //уменьшим дни
+console.log(worker.getSalary()); //выведет 200 - то есть 20*10 */
+
+function Worker(name, surname, rate, days) {
+    this._name = name;
+    this._surname = surname;
+    this._rate = rate;
+    this._days = days;
+};
+
+Worker.prototype = {
+    getName: function () {
+        return this._name;
+    },
+    getSurname: function () {
+        return this._surname;
+    },
+    getRate: function () {
+        return this._rate;
+    },
+    setRate: function (rate) {
+        this._rate = rate;
+    },
+    getDays: function () {
+        return this._days;
+    },
+    setDays: function (days) {
+        this._days = days;
+    },
+    getSalary: function () {
+        const salary = this._rate * this._days
+        return salary;
+    }
+};
+
+var worker = new Worker('Иван', 'Иванов', 10, 31);
+
+console.log(worker.getRate());
+console.log(worker.getDays());
+console.log(worker.getSalary());
+
+worker.setRate(20);
+worker.setDays(10);
+console.log(worker.getSalary());
