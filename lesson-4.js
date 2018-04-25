97190247f0b44794b15e61af256a71da704cd556
Andrey Kolomoets <kolomoets.and@gmail.com>
2018-04-25 17:33:36 +0300

    update lesson-4.js
    выполнено задание №3, подправлен кодстайл

--- lesson-4.js
+++ lesson-4.js
@@ -90,7 +90,49 @@ worker.setRate(20); //увеличим ставку
 worker.setDays(10); //уменьшим дни
 console.log(worker.getSalary()); //выведет 200 - то есть 20*10 */
 
+function Worker ( name, surname, rate, days ) {
+  this._name = name;
+  this._surname = surname;
+  this._rate = rate;
+  this._days = days;
+};
 
+Worker.prototype = {
+  getName : function () {
+    return this._name;
+  },
+  getSurname : function () {
+    return this._surname;
+  },
+  getRate : function () {
+    return this._rate;
+  },
+  setRate : function (rate) {
+    this._rate = rate;
+  },
+  getDays : function () {
+    return this._days;
+  },
+  setDays : function (days) {
+    this._days = days;
+  },
+  getSalary : function () {
+    const salary = this._rate * this._days
+    return salary;
+  }
+};
+
+var worker = new Worker('Иван', 'Иванов', 10, 31);
+
+console.log(worker.getName());
+console.log(worker.getSurname());
+console.log(worker.getRate());
+console.log(worker.getDays());
+console.log(worker.getSalary());
+
+worker.setRate(20);
+worker.setDays(10);
+console.log(worker.getSalary());
 
 /* Задание №4. Реализуйте класс MyString, который будет иметь следующие методы: статический метод reverse(), 
 который параметром принимает строку, а возвращает ее в перевернутом виде, статический метод ucFirst(), 
