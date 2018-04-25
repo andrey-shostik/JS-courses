039cad4133d75d71e3d08097cabc750abfda7c5e
Andrey Kolomoets <kolomoets.and@gmail.com>
2018-04-25 12:22:10 +0300

    update lesson-4.js
    выполнено задание №2

--- lesson-4.js
+++ lesson-4.js
@@ -42,7 +42,41 @@ console.log(worker.getRate()); //выведет 10
 console.log(worker.getDays()); //выведет 31
 console.log(worker.getSalary()); //выведет 310 - то есть 10*31 */
 
+function Worker ( name, surname, rate, days ) {
+  this._name = name;
+  this._surname = surname;
+  this._rate = rate;
+  this._days = days;
+};
 
+Worker.prototype.getName = function () {
+  return this._name;
+};
+
+Worker.prototype.getSurname = function () {
+  return this._surname;
+};
+
+Worker.prototype.getRate = function () {
+  return this._rate;
+};
+
+Worker.prototype.getDays = function () {
+  return this._days;
+};
+
+Worker.prototype.getSalary = function() {
+  const salary = this._rate * this._days
+  return salary;
+};
+
+var worker = new Worker('Иван', 'Иванов', 10, 31);
+
+console.log(worker.getName());
+console.log(worker.getSurname());
+console.log(worker.getRate());
+console.log(worker.getDays());
+console.log(worker.getSalary());
 
 /* Задание №3. Модифицируйте класс Worker из предыдущей задачи следующим образом: 
 для свойства rate и для свойства days сделайте еще и методы-сеттеры. Наш класс теперь будет работать так:
