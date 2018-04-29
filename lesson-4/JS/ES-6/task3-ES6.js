8bc951a9ca33457ea7aca785314532d65b66507f
Olexandr Gorkavenko <gorkavenko.olexandr@gmail.com>
2018-04-29 21:15:26 +0300

    Добавил решение task3-ES6.js

---
+++ lesson-4/JS/ES-6/task3-ES6.js
@@ -0,0 +1,70 @@
+// Задание 3. ES6
+
+/*
+* Модифицируйте класс Worker из предыдущей задачи следующим образом:
+* для свойства rate и для свойства days сделайте еще и методы-сеттеры.
+* Наш класс теперь будет работать так:
+*
+* const worker = new Worker('Иван', 'Иванов', 10, 31);
+*
+* console.log(worker.getRate()); //выведет 10
+* console.log(worker.getDays()); //выведет 31
+* console.log(worker.getSalary()); //выведет 310 - то есть 10*31
+*
+* Теперь используем сеттер:
+* worker.setRate(20); //увеличим ставку
+* worker.setDays(10); //уменьшим дни
+* console.log(worker.getSalary()); //выведет 200 - то есть 20*10
+*/
+
+class Worker {
+
+  constructor(name, surname, rate, days) {
+    this._name = name;
+    this._surname = surname;
+    this._rate = rate;
+    this._days = days;
+    }
+
+  getName() {
+    return this._name;
+    };
+
+  getSurname() {
+    return this._surname;
+    };
+
+  getRate() {
+    return this._rate;
+    };
+
+  setRate(newRate) {
+    this._rate = newRate;
+    };
+
+  getDays() {
+    return this._days;
+    };
+
+  setDays(newDays) {
+    this._days = newDays;
+    };
+
+  getSalary() {
+    return this._rate * this._days;
+    };
+}
+
+const worker = new Worker('Иван', 'Иванов', 10, 31);
+
+console.log('Задание 3. ES6');
+
+console.log( worker.getName() ); //выведет 'Иван'
+console.log( worker.getSurname() ); //выведет 'Иванов'
+console.log( worker.getRate() ); //выведет 10
+console.log( worker.getDays() ); //выведет 31
+console.log( worker.getSalary() ); //выведет 310 - то есть 10*31
+
+worker.setRate(20); //увеличим ставку
+worker.setDays(10); //уменьшим дни
+console.log( worker.getSalary() ); //выведет 200 - то есть 20*10
