6c5e7971ea0d499dc29f6da4aa6a655461b8f266
ElistratovSergey <elistratovs@gmaol.com>
2018-04-30 16:15:57 +0300

    Добавил решение task2-ES5.

---
+++ lesson-4/JS/ES5/task2-ES5.js
@@ -0,0 +1,48 @@
+/*
+Модифицируйте класс Worker из предыдущей задачи следующим образом:
+сделайте все его свойства приватными, а для их чтения сделайте методы-геттеры.
+Наш класс теперь будет работать так:
+
+const worker = new Worker('Иван', 'Иванов', 10, 31);
+
+console.log(worker.getName()); //выведет 'Иван'
+console.log(worker.getSurname()); //выведет 'Иванов'
+console.log(worker.getRate()); //выведет 10
+console.log(worker.getDays()); //выведет 31
+console.log(worker.getSalary()); //выведет 310 - то есть 10*31
+*/
+
+function Worker(name, surname, rate, days) {
+    this._name = name;
+    this._surname = surname;
+    this._rate = rate;
+    this._days = days;
+
+    this.getName = function() {
+        return this._name;
+    };
+
+    this.getSurname = function() {
+        return this._surname;
+    };
+
+    this.getRate = function() {
+        return this._rate;
+    };
+
+    this.getDays = function() {
+        return this._days;
+    };
+
+    this.getSalary = function() {
+        return this._rate * this._days;
+    };
+}
+
+const worker = new Worker('Иван', 'Иванов', 10, 31);
+
+console.log( worker.getName() ); //выведет 'Иван'
+console.log( worker.getSurname() ); //выведет 'Иванов'
+console.log( worker.getRate() ); //выведет 10
+console.log( worker.getDays() ); //выведет 31
+console.log( worker.getSalary() ); //выведет 310 - то есть 10*31
