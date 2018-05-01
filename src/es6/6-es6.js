8050bcc56e1425b7f156c89aa823b62a0bffc418
dmitrijmk <dmitrijmk@gmail.com>
2018-05-01 22:01:22 +0300

    task 6 es6

--- src/es6/6-es6.js
+++ src/es6/6-es6.js
@@ -6,34 +6,72 @@
 // реализовать кэширование. Непосредственно в классе(не в прототипе) BaseValidator реализовать метод getCachedValue,
 // который будет брать закэшированное значение из нужного класса и возвращать его вместо вычеслений.
 
-class BaseValidator {
-    validate(str) {
-        if(/^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/.test(str)){
+class BaseValidator extends Validator {
+    constructor() {
+        super();
+        this.cache = {};
+    }
 
-        }
-        // super.isDomain();
-        // super.isDate();
-        // super.isPhone();
-            }
+    getCachedValue() {
+        return this.cache;
+    };
 }
 
-class EmailValidator extends BaseValidator {
-    validate(str) {
-        return super.validate(str);
+BaseValidator.prototype.validate = () => {};
 
+class EmailValidator extends BaseValidator {
+    validate(email) {
+        if (super.isEmail(email)) {
+            this.cache.email = email;
+
+            return true;
+        }
+    }
+}
+
+class DomainValidator extends BaseValidator {
+    validate(domain) {
+        if (super.isDomain(domain)) {
+            this.cache.domain = domain;
+
+            return true;
+        }
+    }
+}
+
+class DateValidator extends BaseValidator {
+    validate(date) {
+        if (super.isDate(date)) {
+            this.cache.date = date;
+
+            return true;
+        }
+    }
+}
+
+class PhoneValidator extends BaseValidator {
+    validate(phone) {
+        if (super.isPhone(phone)) {
+            this.cache.phone = phone;
+
+            return true;
+        }
     }
 }
 
 const emailValidator = new EmailValidator();
-// const domainValidator = new DomainValidator();
-// const dateValidator = new DateValidator();
-// const phoneValidator = new PhoneValidator();
+const domainValidator = new DomainValidator();
+const dateValidator = new DateValidator();
+const phoneValidator = new PhoneValidator();
+const baseValidator = new BaseValidator();
 
 console.group('6');
 console.log(emailValidator.validate('phphtml@mail.ru'));
 console.log(domainValidator.validate('phphtml.net'));
 console.log(dateValidator.validate('12.05.2020'));
 console.log(phoneValidator.validate('+375 (29) 817-68-92')); //тут можете формат своей страны
-console.group('6');
-//PS. Реализовать на ES5 функциях-конструкторах и на ES6 class.
-// Т.е создаем под одну задачу два фала, например validator-es6.js, validator-es5.js
+console.groupEnd('6');
+
+console.group('6.1');
+console.log(emailValidator.getCachedValue('phphtml@mail.ru'));
+console.groupEnd('6.1');
