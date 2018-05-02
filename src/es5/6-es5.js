2609aa5386c4117bc1f9c6726fa5b77bfb293962
dmitrijmk <dmitrijmk@gmail.com>
2018-05-02 14:09:55 +0300

    task 5 es5

---
+++ src/es5/6-es5.js
@@ -0,0 +1,57 @@
+BaseValidator.prototype = new Validator();
+
+function BaseValidator() {
+    this.cache = {};
+
+    this.getCachedValue = function (value) {
+        return this.cache[value];
+    }
+}
+
+BaseValidator.prototype.validate = function () {
+};
+
+EmailValidator.prototype = new BaseValidator();
+
+function EmailValidator() {
+    this.validate = function (email) {
+        return (email in this.cache) ? this.getCachedValue(email) : (this.cache[email] = this.isEmail(email));
+    }
+}
+
+DomainValidator.prototype = new BaseValidator();
+
+function DomainValidator() {
+    this.validate = function (domain) {
+        return (domain in this.cache) ? this.getCachedValue(domain) : (this.cache[domain] = this.isDomain(domain));
+    }
+}
+
+DateValidator.prototype = new BaseValidator();
+
+function DateValidator() {
+    this.validate = function (date) {
+        return (date in this.cache) ? this.getCachedValue(date) : (this.cache[date] = this.isDate(date));
+    }
+}
+
+PhoneValidator.prototype = new BaseValidator();
+
+function PhoneValidator() {
+    this.validate = function (phone) {
+        return (phone in this.cache) ? this.getCachedValue(phone) : (this.cache[phone] = this.isPhone(phone));
+    }
+}
+
+const emailValidator = new EmailValidator();
+const domainValidator = new DomainValidator();
+const dateValidator = new DateValidator();
+const phoneValidator = new PhoneValidator();
+
+console.group('6');
+console.log(emailValidator.validate('phphtml@mail.ru'));
+console.log(domainValidator.validate('phphtml.net'));
+console.log(dateValidator.validate('12.05.2020'));
+console.log(phoneValidator.validate('+375 (29) 817-68-92')); //тут можете формат своей страны
+console.groupEnd('6');
+
--- src/es6/6-es6.js
+++ src/es6/6-es6.js
@@ -1,11 +1,3 @@
-// 6. Реализуйте класс BaseValidator, который будет иметь один публичный метод в прототипе validate.
-// Также нужно реализовать классы EmailValidator, DomainValidator, DateValidator, PhoneValidator,
-// которые будут наследоваться от BaseValidator и определять метод validate каждый по-своему.
-
-// Дополнительное задание: в классах(не в прототипах) EmailValidator, DomainValidator, DateValidator, PhoneValidator
-// реализовать кэширование. Непосредственно в классе(не в прототипе) BaseValidator реализовать метод getCachedValue,
-// который будет брать закэшированное значение из нужного класса и возвращать его вместо вычеслений.
-
 class BaseValidator extends Validator {
     constructor() {
         super();
@@ -21,25 +13,25 @@ BaseValidator.prototype.validate = function () {};
 
 class EmailValidator extends BaseValidator {
     validate(email) {
-        return (email in this.cache) ? super.getCachedValue(email) : (this.cache[email] = this.isEmail(email));
+        return (email in this.cache) ? this.getCachedValue(email) : (this.cache[email] = this.isEmail(email));
     }
 }
 
 class DomainValidator extends BaseValidator {
     validate(domain) {
-        return (domain in this.cache) ? super.getCachedValue(domain) : (this.cache[domain] = super.isDomain(domain));
+        return (domain in this.cache) ? this.getCachedValue(domain) : (this.cache[domain] = this.isDomain(domain));
     }
 }
 
 class DateValidator extends BaseValidator {
     validate(date) {
-        return (date in this.cache) ? super.getCachedValue(date) : (this.cache[date] = super.isDate(date));
+        return (date in this.cache) ? this.getCachedValue(date) : (this.cache[date] = this.isDate(date));
     }
 }
 
 class PhoneValidator extends BaseValidator {
     validate(phone) {
-        return (phone in this.cache) ? super.getCachedValue(phone) : (this.cache[phone] = super.isPhone(phone));
+        return (phone in this.cache) ? this.getCachedValue(phone) : (this.cache[phone] = this.isPhone(phone));
     }
 }
 
