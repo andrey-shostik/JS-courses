82eb5ae21f47c970b1f33f597e3652e5181f217e
TereschenkoVlad <vlad.tereshchenko1999@gmail.com>
2018-05-14 14:58:06 +0300

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "add task#6""""""""""""
    This reverts commit 875e794564bddd1843aec8aa69fd6d093cf0d323.

---
+++ Lesson-6/main.js
@@ -0,0 +1,63 @@
+
+//
+//
+// task #1
+
+function sumArguments(value) {
+    return secondValue => nextValue => value + secondValue + nextValue;
+}
+
+
+console.log(sumArguments(1)(2)(3) === 6);
+
+//
+//
+// task #2.1
+function sumArg(value) {
+    return nextValue => nextValue ? sumArg(value + nextValue) : value;
+}
+
+
+console.log(sumArg(1)(2)(3)(4)(5)() === 15);
+//
+//
+// task #2.2
+
+function secondSumArg(value) {
+    const totalSum = nextValue => secondSumArg(value + nextValue);
+
+    totalSum.toString = () => value;
+
+    return totalSum;
+}
+
+alert(secondSumArg(1)(2)(3)(4)(5)(6));
+
+
+
+//
+//
+// task #3
+function makeCounter() {
+    let initialValue = 0;
+
+    const counter = () => ++initialValue;
+
+    counter.set = (value) => {
+        initialValue = --value;
+    };
+    counter.reset = () => {
+        initialValue = 0;
+    };
+
+    return counter;
+}
+
+
+const counter = makeCounter();
+counter(); // 1
+counter(); // 2
+counter.set(10);
+counter(); // 12
+counter.reset();
+counter(); // 1
