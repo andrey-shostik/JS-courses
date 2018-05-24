c5e34f7f81db187e1575b6a461874bf49964d34a
Medvedenko Yaroslav <medvedenko96@gmail.com>
2018-05-24 14:22:52 +0300

    add Homework DOM

--- main.js
+++ main.js
@@ -1,51 +1,90 @@
+function crateInput() {
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+    const inputRow    = document.createElement('input');
+    const inputColumn = document.createElement('input');
 
+    inputRow.setAttribute('type', 'text');
+    inputColumn.setAttribute('type', 'text');
 
+    inputColumn.placeholder = 'row';
+    inputRow.placeholder = 'column';
 
+    inputColumn.classList.add('input-table');
+    inputRow.classList.add('input-table');
 
-function renderTable(quantityRow = 10, quantityCell = 10) {
-        const table  = document.createElement('table');
-        const button = document.createElement('button');
+    container.appendChild(inputColumn);
+    container.appendChild(inputRow);
 
-            for (let i = 0; i < quantityRow; i++) {
-                const tr = table.insertRow(0);
+    const buttonCreateTable = document.createElement('button');
+    buttonCreateTable.innerHTML = 'Create table';
+    buttonCreateTable.classList.add('input-table');
+    container.appendChild(buttonCreateTable);
 
-                    for (let j = 0; j < quantityCell; j++) {
-                        const td = tr.insertCell(0);
+    const allInput = document.getElementsByClassName('input-table');
 
-                        td.style.border = '1px solid #777';
-                        td.style.width  = '25px';
-                        td.style.height = '25px';
-                    }
-            }
-
-        button.style.marginTop     = '10px';
-        button.innerHTML           = 'Поменять цвета';
-        table.style.borderCollapse = 'collapse';
-
-            table.addEventListener('click', e => {
-                    if (e.target.tagName === 'TD') applyColor(e.target);
-                });
-
-            button.addEventListener('click', () => {
-                    const allTd = table.getElementsByTagName('td');
-
-                        for (let i = 0; i < allTd.length; i++) {
-                            applyColor(allTd[i]);
-                        }
-                });
-
-            document.body.appendChild(table);
-        document.body.appendChild(button);
+    for (let i = 0; i < allInput.length; i++) {
+        allInput[i].style.marginRight = '10px';
+        allInput[i].style.lineHeight = '17px';
+        allInput[i].style.border = '1px solid gray';
     }
 
-function applyColor(value) {
-       if (value.className !== 'black') {
-                value.style.background = 'black';
-                value.className        = 'black';
-            } else {
-                value.style.background = 'white';
-                value.className        = 'white';
-            }
+    buttonCreateTable.addEventListener('click', () => {
+        crateTable(inputColumn.value, inputRow.value)
+    });
+}
+
+function crateTable(row, column) {
+
+    const containerTable = document.createElement('div');
+    containerTable.id = 'current-table';
+    document.body.appendChild(containerTable);
+    containerTable.style.marginTop = '10px';
+
+
+    const table  = document.createElement('table');
+    const button = document.createElement('button');
+
+    for (let r = 0; r < row; r++) {
+        const tr = table.insertRow(0);
+
+        for (let c = 0; c < column; c++) {
+            const td = tr.insertCell(0);
+            td.style.border = '2px solid gray';
+            td.style.width  = '25px';
+            td.style.height = '25px';
+        }
     }
 
-renderTable();
+    table.addEventListener('click', event => {
+        if (event.srcElement.nodeName === 'TD')
+            changeСolorTd(event.srcElement);
+    });
+
+    button.addEventListener('click', () => {
+        const allTd = table.getElementsByTagName('td');
+
+        for (let i = 0; i < allTd.length; i++) {
+            changeСolorTd(allTd[i]);
+        }
+    });
+
+    table.style.borderCollapse = 'collapse';
+    button.style.marginTop     = '10px';
+    button.innerHTML           = 'Change colors';
+    table.style.borderCollapse = 'collapse';
+    containerTable.appendChild(table);
+    containerTable.appendChild(button);
+
+    function changeСolorTd(node) {
+        if (node.className !== 'black') {
+            node.style.background = 'black';
+            node.className        = 'black';
+        } else {
+            node.style.background = 'white';
+            node.className        = 'white';
+        }
+    }
+}
+
+crateInput();
