e3e36b97e9e20b23ed7f6df65a263d5f95fd7bad
dmitrijmk <dmitrijmk@gmail.com>
2018-05-28 15:37:22 +0300

    диструктурировал ответ с сервера

--- js/task2.js
+++ js/task2.js
@@ -2,7 +2,7 @@ const httpGet = url => fetch(url)
     .then(response => response.json());
 
 const getDataMethodPromise = (url1, url2) => httpGet(url1)
-    .then(response => response['getUsersData'] ? httpGet(url2)
+    .then(({getUsersData}) => getUsersData ? httpGet(url2)
         .then(result => console.log(result)) : console.error('error'));
 
 const getDataMethodAsyncAwait = async (url1, url2) =>
