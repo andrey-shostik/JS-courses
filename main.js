618c30f30168c0e748ddcae12b131c0a10ae68dc
Medvedenko Yaroslav <medvedenko96@gmail.com>
2018-05-29 13:40:08 +0300

    add

--- main.js
+++ main.js
@@ -6,25 +6,56 @@ function delay(ms) {
 
 delay(100).then(() => console.log("Hello!"));
 
-
 const fetch = require('node-fetch');
 const url = 'http://www.json-generator.com/api/json/get/cfQCylRjuG';
 const urlUsers = 'http://www.json-generator.com/api/json/get/cfVGucaXPC';
 
-function getUserData() {
+/*(function getUserDataProm(url, urlUsers) {
     fetch(url, {method: 'GET'})
         .then(res => res.json())
-        .then(function (json) {
-            if (json.getUsersData) {
+        .then(json => {
+            const {getUsersData} = json;
+            if (getUsersData === true) {
                 fetch(urlUsers, {method: 'GET'})
                     .then(res => res.json())
                     .then(json => console.log(json))
+                    .catch(e => console.log(e))
             }
         });
-}
+})(url, urlUsers);
 
-getUserData();
+const getUserDataAsync = async (url, urlUsers) => {
+    try {
+     const {getUsersData} = await (await fetch(url, {method: 'GET'})).json();
+        if (getUsersData === true) {
+            console.log(await (await  fetch(urlUsers, {method: 'GET'})).json())
+        }
+    } catch (e) {
+        console.log(e)
+    }
+};
 
-function getUsreDataAsync() {
-    
+getUserDataAsync(url, urlUsers);*/
+
+const urls = [
+        'http://www.json-generator.com/api/json/get/cevhxOsZnS',
+        'http://www.json-generator.com/api/json/get/cguaPsRxAi',
+        'http://www.json-generator.com/api/json/get/cfDZdmxnDm',
+        'http://www.json-generator.com/api/json/get/cfkrfOjrfS',
+        'http://www.json-generator.com/api/json/get/ceQMMKpidK'
+];
+
+
+function getJsons(urls) {
+    let result = [];
+    for (let i = 0; i < urls.length; i++){
+        fetch(urls[i], {method : 'GET'})
+            .then(res => res.json())
+            .then(json => result.push(json))
+            //.then(json => console.log(json))
+            .catch(e => console.log(e))
+    }
+
+    return result
 }
+console.log(getJsons(urls));
