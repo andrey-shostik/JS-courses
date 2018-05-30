4e25ce2c983258b724b82190c9fff2b336bf2792
BilousAlexandr <bilousbrainbasket@gmail.com>
2018-05-30 16:59:58 +0300

    lesson-6

--- hw-4/homework4.js
+++ hw-4/homework4.js
@@ -1,10 +1,8 @@
-//task 1-2
+//task 1
 function sum (x) {
     let targetSum = x
     function getNewSum (y) {
-        if (y == undefined) {
-            return getNewSum
-        }
+
         targetSum += y
         return getNewSum
     }
@@ -13,4 +11,29 @@ function sum (x) {
     }
     return getNewSum
 }
-alert(sum(1)(2)(10)())
+alert(sum(1)(2)(10))
+//task 2
+function sum (x) {
+    let targetSum = x
+    function getNewSum (y) {
+        if (y)
+        targetSum += y
+        return getNewSum
+    }
+    getNewSum.toString = function () {
+        return targetSum
+    }
+    return getNewSum
+}
+alert(sum(1)(2)(7)())
+
+//task 3
+const makeCounter = () => {
+    let currentCount = 1
+    const counter  = () => { return currentCount++ }
+    counter.set = (getValue) => { currentCount = getValue }
+    counter.reset = () => { currentCount = 1}
+    return counter;
+}
+const counter = makeCounter()
+console.log(counter())
